0a368916aa575ea2dad96768231ab20f5843af4c
Gabriel Vidile <98036449+gabriel-vidile@users.noreply.github.com>
2024-01-28 00:49:30 -0300

    atualiznaod

--- api/supabase.js
+++ api/supabase.js
@@ -1,7 +1,7 @@
 import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://mebdvqkicqmelyhtucat.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lYmR2cWtpY3FtZWx5aHR1Y2F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3OTEyMDMsImV4cCI6MjAxNjM2NzIwM30.-LOPLv9-o0MBBGojQR-LWQPC4zCouPDfZJGTE_FTiq4"
+const supabaseUrl = process.env.SUPABASEURL
+const supabaseAnonKey = process.env.SUPABASEANONKEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
